e9f197023c3715617a6cb34c10caaef631affd2b
RANDRIAMIHAINGO Herinandrianina Eloi Charly <nandry556@gmail.com>
2025-02-26 00:48:00 +0300

    feat: app base symfony with security

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index( EntityManagerInterface $entityManager,UserPasswordHasherInterface  $hasher): Response
     {
+            $this->denyAccessUnlessGranted('ROLE_USER'); 
+            // $user = (new  User())
+            // ->setEmail('nandry556@gmail.com')
+            // ->setUsername('Eloi Charly')
+            // ->setRoles(['ROLE_USER']); 
+            // $user =  $user->setPassword($hasher->hashPassword($user, '1111'));
+            // $entityManager->persist($user);
+            // $entityManager->flush();
+
         return $this->render('home/index.html.twig', 
         ['message' => 'Welcome to your new controller!']
     );
